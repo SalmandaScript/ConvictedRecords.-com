ef6e447a18e5d3a33c4f99bcfefb2d3a60d466ff
SalmandaScript <110246267+SalmandaScript@users.noreply.github.com>
2024-01-29 01:01:07 +0200

    Add files via upload

---
+++ artist.js
@@ -0,0 +1,59 @@
+{
+const textarea =document.querySelector ("textarea");
+h2 =document.querySelector("h2");
+
+textarea.addEventListener ("keyup", display);
+
+function display(){
+h2.innerHTML=textarea.value;
+}
+}
+
+function feedBack(){
+if (document.TextArea.TextArea.value=="string");
+{
+alert(" Thanks for engaging with us your  information will be reviewed, if you've send us your contact details, we will surely get back to you✊💝.");
+}
+return true;
+}
+
+$(document).ready (function(){
+$("img").attr("loading", "lazy");
+});
+
+
+//salmanda  links
+function salmandaFacebook(){
+open("https://www.facebook.com/profile.php?id=100009564969932");
+}
+
+function salmandaTelegram(){
+open("https://t.me/Salmandamusiq");
+}
+
+function niceTimeMixtape(){
+open("https://audiomack.com/clementjumacmm/album/nice-time-the-mixtape-1");
+}
+
+function africaOnline(){
+open("https://salmandascript.github.io/AfricaOnline/");
+}
+
+
+//salmanda links end 
+
+ /*function likeButton(){
+document.getElementById("like"). style. background ="maroon";
+}
+
+function viewButton(){
+document.getElementById("view"). style. background ="maroon";
+}
+
+function likeButton2(){
+document.getElementById("like"). style. background ="orange";
+}
+
+function viewButton2(){
+document.getElementById("view"). style. background ="orange";
+}*/
---
+++ contact.js
@@ -0,0 +1,9 @@
+document.getElementById ("demo"). innerHTML="For Further inquiries, contact us via any of these Apps.";
+
+function facebook(){
+open("https://www.facebook.com/profile.php?id=100009564969932");
+}
+
+function telegram(){
+open("https://t.me/Salmandamusiq");
+}
